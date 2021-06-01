9affdc3533d0fd9d65a63071fa282b8e9cc7295c
martinorbanz-a9s <69192969+martinorbanz-a9s@users.noreply.github.com>
2021-06-01 17:44:23 +0200

    add headers to loadAfterBeforeLoad

--- src/@ionic-native/plugins/in-app-browser-anynines/index.ts
+++ src/@ionic-native/plugins/in-app-browser-anynines/index.ts
@@ -182,9 +182,11 @@ export class InAppBrowserObject {
   /**
    * Method to be called after the "beforeload" event to continue the script
    * @param strUrl {String} The URL the InAppBrowser should navigate to.
+   * @param headers {String | Record<string, string} Either a serialized string or JS object (hash map)
+   * containing custom headers to be sent with the request
    */
   @CordovaInstance({ sync: true })
-  _loadAfterBeforeload(strUrl: string): void {}
+  loadAfterBeforeload(strUrl: string, headers?: string | Record<string, string>): void {}
 
   /**
    * Displays an InAppBrowser window that was opened hidden. Calling this has no effect
@@ -207,11 +209,10 @@ export class InAppBrowserObject {
   hide(): void {}
 
   /**
-   * Returns the WKWebView's canGoBack bool value.
-   * True if there is more than one item in the browsing history
+   * Takes two callbacks for success and error, where success recieves the WebView's canGoBack boolean value
    */
   @CordovaInstance({ sync: true })
-  canGoBack(): void {}
+  canGoBack(successCallback: (result: boolean) => void, errorCallback: (error: Error | string) => void): void {}
 
   /**
    * Calls the InAppBrowser's goBack function (history)
